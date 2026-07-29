b7ad9f518604e56ed0b1f0e6707b20d3b822d227
notnotmelon <zacharypicco@gmail.com>
2026-07-29 16:15:04 -0700

    recipes

--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -1,31 +1,6 @@
-local categories = data.raw.recipe["speed-module"].categories
+local categories = data.raw.recipe["speed-module"] and data.raw.recipe["speed-module"].categories or nil
 
-local parallel_module_3_ingredients = {
-  {
-    amount = 4,
-    name = "parallel-module-2",
-    type = "item"
-  },
-  {
-    amount = 5,
-    name = "advanced-circuit",
-    type = "item"
-  },
-  {
-    amount = 5,
-    name = "processing-unit",
-    type = "item"
-  }
-}
-if mods["space-age"] then
-  table.insert(parallel_module_3_ingredients, {
-    amount = 1,
-    name = "uranium-235",
-    type = "item"
-  })
-end
-
-data:extend({
+local fallbacks = {
   {
     name = "parallel-module",
     type = "recipe",
@@ -44,13 +19,6 @@ data:extend({
         type = "item"
       }
     },
-    results = {
-      {
-        amount = 1,
-        name = "parallel-module",
-        type = "item"
-      }
-    }
   },
   {
     name = "parallel-module-2",
@@ -75,13 +43,6 @@ data:extend({
         type = "item"
       }
     },
-    results = {
-      {
-        amount = 1,
-        name = "parallel-module-2",
-        type = "item"
-      }
-    }
   },
   {
     name = "parallel-module-3",
@@ -89,13 +50,51 @@ data:extend({
     categories = categories,
     enabled = false,
     energy_required = 60,
-    ingredients = parallel_module_3_ingredients,
-    results = {
+    ingredients = {
       {
-        amount = 1,
-        name = "parallel-module-3",
+        amount = 4,
+        name = "parallel-module-2",
         type = "item"
-      }
+      },
+      {
+        amount = 5,
+        name = "advanced-circuit",
+        type = "item"
+      },
+      {
+        amount = 5,
+        name = "processing-unit",
+        type = "item"
+      },
+      mods["space-age"] and {
+        amount = 1,
+        name = "quantum-processor",
+        type = "item"
+      } or nil
+    },
+  }
+}
+
+for i, suffix in pairs{"", "-2", "-3"} do
+  local new_recipe = table.deepcopy(data.raw.recipe["speed-module" .. suffix] or fallbacks[i])
+  new_recipe.name = "parallel-module" .. suffix
+  new_recipe.results = {
+    {
+      amount = 1,
+      name = "parallel-module-3",
+      type = "item"
     }
-  },
-})
+  }
+  for _, ingredient in pairs(new_recipe.ingredients) do
+    if ingredient.name == "speed-module" then
+      ingredient.name = "parallel-module"
+    elseif ingredient.name == "speed-module-2" then
+      ingredient.name = "parallel-module-2"
+    elseif ingredient.name == "speed-module-3" then
+      ingredient.name = "parallel-module-3"
+    elseif ingredient.name == "tungsten-carbide" then
+      ingredient.name = "quantum-processor"
+    end
+  end
+  data:extend{new_recipe}
+end
