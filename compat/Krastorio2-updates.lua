0d741e268ff9dea2d87dae1f00838e3c63eeb180
notnotmelon <zacharypicco@gmail.com>
2026-07-29 16:17:53 -0700

    k2 compat

---
+++ compat/Krastorio2-updates.lua
@@ -0,0 +1,9 @@
+
+if not mods["Krastorio2-spaced-out"] then
+    local data_util = require("__Krastorio2__.data-util")
+    data_util.add_prerequisite("parallel-module-2", "chemical-science-pack")
+    data_util.add_prerequisite("parallel-module-3", "processing-unit")
+    data_util.add_prerequisite("parallel-module-3", "production-science-pack")
+
+    data_util.remove_prerequisite("parallel-module-2", "processing-unit")
+end
