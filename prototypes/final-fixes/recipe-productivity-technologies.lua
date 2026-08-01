82fa0c492a9f3fe3707429a9fe9538f130c9dbbf
notnotmelon <zacharypicco@gmail.com>
2026-08-01 07:26:10 -0700

    simplify recipe-productivity-technologies.lua

--- prototypes/final-fixes/recipe-productivity-technologies.lua
+++ prototypes/final-fixes/recipe-productivity-technologies.lua
@@ -5,41 +5,15 @@ for _, technology in pairs(data.raw.technology) do
         goto continue
     end
 
-    local base_unlock_recipes = {}
     local base_productivity_recipes = {}
     for _, effect in pairs(technology.effects) do
-        if effect.type == "unlock-recipe" then
-            table.insert(base_unlock_recipes, effect.recipe)
-        elseif effect.type == "change-recipe-productivity" then
+        if effect.type == "change-recipe-productivity" then
             base_productivity_recipes[effect.recipe] = effect.change
         end
     end
 
-    for _, base_recipe in pairs(base_unlock_recipes) do
-        local altered_recipes = base_recipe_to_altered_recipes[base_recipe]
-        if altered_recipes == nil then
-            goto continue
-        end
-
-        for _, altered_recipe in pairs(altered_recipes) do
-            if altered_recipe ~= base_recipe then
-                table.insert(technology.effects, {
-                    type   = "unlock-recipe",
-                    recipe = altered_recipe,
-                    hidden = true
-                })
-            end
-        end
-        ::continue::
-    end
-
     for base_recipe, change in pairs(base_productivity_recipes) do
-        local altered_recipes = base_recipe_to_altered_recipes[base_recipe]
-        if altered_recipes == nil then
-            goto continue
-        end
-
-        for _, altered_recipe in pairs(altered_recipes) do
+        for _, altered_recipe in pairs(base_recipe_to_altered_recipes[base_recipe] or {}) do
             if altered_recipe ~= base_recipe then
                 table.insert(technology.effects, {
                     type   = "change-recipe-productivity",
@@ -49,7 +23,6 @@ for _, technology in pairs(data.raw.technology) do
                 })
             end
         end
-        ::continue::
     end
     ::continue::
 end
