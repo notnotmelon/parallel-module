cab24287b265a036650ee222b59f5ca6e51cc230
notnotmelon <zacharypicco@gmail.com>
2026-08-03 10:10:17 -0700

    formatting

--- prototypes/mod-data.lua
+++ prototypes/mod-data.lua
@@ -1,28 +1,19 @@
-data:extend {
-    ---------------------------------------------------------------------------
-    --- SETTINGS AND COMPATIBILITY
-    ---------------------------------------------------------------------------
-    --- NOTE for other modders: Instructions are provided in comments for if/how
-    --- your mod should alter these values for compatibility. This should happen
-    --- in your mod's data-updates.lua
-    ---------------------------------------------------------------------------
-    {
-        name = "parallel-module",
-        type = "mod-data",
-        data = {
-            -- internal use only
-            allowed_machines = {},
-            allowed_recipes = {},
-            recipe_table = {},
-            recipe_table_inverse = {},
-            parallel_value_cache = {},
-            spoilable_modules = {},
-            entity_to_base_parallel = {},
-            crafting_machine_types = {
-                "assembling-machine",
-                "rocket-silo",
-                "furnace",
-            },
+data:extend {{
+    name = "parallel-module",
+    type = "mod-data",
+    -- internal use only, do not edit these
+    data = {
+        allowed_machines = {},
+        allowed_recipes = {},
+        recipe_table = {},
+        recipe_table_inverse = {},
+        parallel_value_cache = {},
+        spoilable_modules = {},
+        entity_to_base_parallel = {},
+        crafting_machine_types = {
+            "assembling-machine",
+            "rocket-silo",
+            "furnace",
         },
     },
-}
+}}
