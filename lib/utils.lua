b60b23a514da954ab86b93d9f25bdefbb50931e5
notnotmelon <zacharypicco@gmail.com>
2026-08-05 23:10:49 -0700

    more strict parallel rounding

--- lib/utils.lua
+++ lib/utils.lua
@@ -11,16 +11,20 @@ end
 
 -- round parallel amounts to keep the total number of added recipes small
 utils.round_parallel = function(parallel)
-    if parallel <= 24 then
+    if parallel <= 12 then
         return math.ceil(parallel)
     end
 
-    if parallel <= 48 then
+    if parallel <= 24 then
         return math.ceil(parallel / 2) * 2
     end
 
-    if parallel <= 96 then
+    if parallel <= 48 then
         return math.ceil(parallel / 4) * 4
+    end
+
+    if parallel <= 96 then
+        return math.ceil(parallel / 8) * 8
     end
 
     local step = 10 ^ math.floor(math.log(parallel, 10) - 1)
--- settings.lua
+++ settings.lua
@@ -10,7 +10,7 @@ data:extend {{
     type = "int-setting",
     name = "parallel-module-max-ingredient-product-amount",
     setting_type = "startup",
-    default_value = 5000,
+    default_value = 2000,
     miniumum_value = 100,
     maximum_value = 65535,
     order = "b",
