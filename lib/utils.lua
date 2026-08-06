14f0101e317a22e76041afdd113902eb9dabb9f0
notnotmelon <zacharypicco@gmail.com>
2026-08-06 02:47:44 -0700

    added parallel-module-rounding-strictness setting

--- lib/utils.lua
+++ lib/utils.lua
@@ -10,25 +10,64 @@ utils.table_contains_value = function(table, value)
 end
 
 -- round parallel amounts to keep the total number of added recipes small
-utils.round_parallel = function(parallel)
-    if parallel <= 12 then
+local precision = settings.startup["parallel-module-rounding-strictness"].value
+if precision == "12x" then
+    utils.round_parallel = function(parallel)
+        if parallel <= 12 then
+            return math.ceil(parallel)
+        end
+
+        if parallel <= 24 then
+            return math.ceil(parallel / 2) * 2
+        end
+
+        if parallel <= 48 then
+            return math.ceil(parallel / 4) * 4
+        end
+
+        if parallel <= 96 then
+            return math.ceil(parallel / 8) * 8
+        end
+
+        local step = 10 ^ math.floor(math.log(parallel, 10) - 1)
+        return math.floor(parallel / step) * step
+    end
+elseif precision == "24x" then
+    utils.round_parallel = function(parallel)
+        if parallel <= 24 then
+            return math.ceil(parallel)
+        end
+
+        if parallel <= 48 then
+            return math.ceil(parallel / 2) * 2
+        end
+
+        if parallel <= 96 then
+            return math.ceil(parallel / 4) * 4
+        end
+
+        local step = 10 ^ math.floor(math.log(parallel, 10) - 1)
+        return math.floor(parallel / step) * step
+    end
+elseif precision == "48x" then
+    utils.round_parallel = function(parallel)
+        if parallel <= 48 then
+            return math.ceil(parallel)
+        end
+
+        if parallel <= 96 then
+            return math.ceil(parallel / 2) * 2
+        end
+
+        local step = 10 ^ math.floor(math.log(parallel, 10) - 1)
+        return math.floor(parallel / step) * step
+    end
+elseif precision == "never-round" then
+    utils.round_parallel = function(parallel)
         return math.ceil(parallel)
     end
-
-    if parallel <= 24 then
-        return math.ceil(parallel / 2) * 2
-    end
-
-    if parallel <= 48 then
-        return math.ceil(parallel / 4) * 4
-    end
-
-    if parallel <= 96 then
-        return math.ceil(parallel / 8) * 8
-    end
-
-    local step = 10 ^ math.floor(math.log(parallel, 10) - 1)
-    return math.floor(parallel / step) * step
+else
+    error()
 end
 
 if data and data.raw then cached_tostring = tostring end
--- settings.lua
+++ settings.lua
@@ -7,13 +7,27 @@ data:extend {{
 }}
 
 data:extend {{
+    type = "string-setting",
+    name = "parallel-module-rounding-strictness",
+    setting_type = "startup",
+    default_value = mods.pyalienlife and "24x" or "never-round",
+    order = "b",
+    allowed_values = {
+        "12x",
+        "24x",
+        "48x",
+        "never-round",
+    },
+}}
+
+data:extend {{
     type = "int-setting",
     name = "parallel-module-max-ingredient-product-amount",
     setting_type = "startup",
     default_value = 1000,
     miniumum_value = 100,
     maximum_value = 65535,
-    order = "b",
+    order = "c",
 }}
 
 data:extend {{
@@ -21,5 +35,5 @@ data:extend {{
     name = "parallel-module-exclude-fluid-recipes",
     setting_type = "startup",
     default_value = false,
-    order = "c",
+    order = "d",
 }}
