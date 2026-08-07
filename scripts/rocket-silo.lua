66078538da1064ff5ad8e0b80609983a6987e2a7
notnotmelon <zacharypicco@gmail.com>
2026-08-07 08:45:54 -0700

    simplify rocket silo logic

--- scripts/rocket-silo.lua
+++ scripts/rocket-silo.lua
@@ -1,13 +1,10 @@
 for rocket_part_recipe, num_parallels in pairs(mod_data.rocket_part_recipes) do
     local prototype = prototypes.recipe[rocket_part_recipe]
+    if not prototype then goto continue end
     parallel.on_event(prototype.on_crafted_event, function(event)
         local rocket_silo = event.entity
         if rocket_silo.type ~= "rocket-silo" then return end
-        local unit_number = rocket_silo.unit_number
-
-        local overflow = storage.rocket_part_overflow[unit_number] or 0
-        local additional = (num_parallels - 1) + overflow
-
-        rocket_silo.rocket_parts = rocket_silo.rocket_parts + additional
+        rocket_silo.rocket_parts = rocket_silo.rocket_parts + num_parallels - 1
     end)
+    ::continue::
 end
